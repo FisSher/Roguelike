a93758a6bbb60b692b9c65c79db81ca03a77499a
Facundo Paolini <39218134+FisSher@users.noreply.github.com>
2020-04-30 19:05:44 -0300

    Hice el scritp del jugador, me faltan los enemigos.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
 	public static GameManager instance = null;
 	public BoardManager boardScript;
+	public int playerFoodPoints = 100;
+	[HideInInspector] public bool playersTurn = true;
+
 
 	private int level = 3;
 
@@ -30,6 +34,11 @@ public class GameManager : MonoBehaviour {
 		boardScript.SetupScene(level);
 	}
 	
+	public void GameOver()
+	{
+		enabled = false;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
