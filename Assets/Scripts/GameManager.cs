e5cc86c8d0d83ab8a4ebe6faed4d48bf00015fc5
Facundo Paolini <39218134+FisSher@users.noreply.github.com>
2020-05-02 14:54:33 -0300

    Terminé la version 1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
-
+	public float levelStartDelay = 2f;
+	public float turnDelay = .1f;
 	public static GameManager instance = null;
 	public BoardManager boardScript;
 	public int playerFoodPoints = 100;
 	[HideInInspector] public bool playersTurn = true;
 
-
-	private int level = 3;
-
+	private Text levelText;
+	private GameObject levelImage;
+	private int level = 1;
+	private List<Enemy> enemies;
+	private bool enemiesMoving;
+	private bool doingSetup;
 
 	// Use this for initialization
 	void Awake () {
@@ -26,21 +32,78 @@ public class GameManager : MonoBehaviour {
 		}
 
 		DontDestroyOnLoad(gameObject);
+		enemies = new List<Enemy>();
 		boardScript = GetComponent<BoardManager>();
 		InitGame();		
 	}
+
+	void OnLevelWasLoaded(int index)
+	{
+		//Add one to our level number.
+		level++;
+		//Call InitGame to initialize our level.
+		InitGame();
+	}
+
+
 	void InitGame()
 	{
+		doingSetup = true;
+		levelImage = GameObject.Find("LevelImage");
+		levelText = GameObject.Find("LevelText").GetComponent<Text>();
+		levelText.text = "Día " + level;
+		levelImage.SetActive(true);
+		Invoke("HideLevelImage", levelStartDelay);
+
+		enemies.Clear();
 		boardScript.SetupScene(level);
+
 	}
+
+	private void HideLevelImage()
+	{
+		levelImage.SetActive(false);
+		doingSetup = false;
+	}
+
 	
 	public void GameOver()
 	{
+		levelText.text = "Sobreviviste " + level + " días.";
+		levelImage.SetActive(true);
 		enabled = false;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
+		if(playersTurn || enemiesMoving || doingSetup)
+		{
+			return;
+		}
+		StartCoroutine(MoveEnemies());
+
 	}
+	public void AddEnemyToList(Enemy script)
+	{
+		enemies.Add(script);
+	}
+
+	IEnumerator MoveEnemies()
+	{
+		enemiesMoving = true;
+		yield return new WaitForSeconds(turnDelay);
+		if (enemies.Count == 0)
+		{
+			yield return new WaitForSeconds(turnDelay);
+		}
+		for (int i = 0; i < enemies.Count; i++)
+		{
+			enemies[i].MoveEnemy();
+			yield return new WaitForSeconds(enemies[i].moveTime);
+		}
+
+		playersTurn = true;
+		enemiesMoving = false;
+	}
+
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,21 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MovingObject {
 	public int wallDamage = 1;
 	public int pointsPerFood = 10;
 	public int pointsPerSoda = 20;
-	public float restartLevelDelay = 1;
+	public float restartLevelDelay = 1f;
+	public Text foodText;
+	public AudioClip moveSound1;
+	public AudioClip moveSound2;
+	public AudioClip eatSound1;
+	public AudioClip eatSound2;
+	public AudioClip drinkSound1;
+	public AudioClip drinkSound2;
+	public AudioClip gameOverSound;
 
 	private Animator animator;
 	private int food;
+
+	//mobile controls
+	private Vector2 touchOrigin = -Vector2.one;
 
 
 	// Use this for initialization
 	protected override void Start () {
 		animator = GetComponent<Animator>();
+
 		food = GameManager.instance.playerFoodPoints;
+
+		foodText.text = "Energía: " + food;
+
 		base.Start();
 	}
 
@@ -25,29 +41,94 @@ public class Player : MovingObject {
 		GameManager.instance.playerFoodPoints = food;
 		
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	private void Update()
+	{
+		//If it's not the player's turn, exit the function.
 		if (!GameManager.instance.playersTurn) return;
-		int horizontal = 0;
-		int vertical = 0;
 
-		horizontal = (int)Input.GetAxisRaw("Horizontal");
-		vertical = (int)Input.GetAxisRaw("Vertical");
+		int horizontal = 0;      //Used to store the horizontal move direction.
+		int vertical = 0;        //Used to store the vertical move direction.
 
+		//Check if we are running either in the Unity editor or in a standalone build.
+#if UNITY_STANDALONE || UNITY_WEBPLAYER
+
+		//Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
+		horizontal = (int)(Input.GetAxisRaw("Horizontal"));
+
+		//Get input from the input manager, round it to an integer and store in vertical to set y axis move direction
+		vertical = (int)(Input.GetAxisRaw("Vertical"));
+
+		//Check if moving horizontally, if so set vertical to zero.
 		if (horizontal != 0)
+		{
 			vertical = 0;
-		
-		if (horizontal != 0 || vertical != 00)
-			AttemptMove<Wall>(horizontal, vertical);
+		}
 
+
+
+#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+
+            //Check if Input has registered more than zero touches
+            if (Input.touchCount > 0)
+            {
+                //Store the first touch detected.
+                Touch myTouch = Input.touches[0];
+
+                //Check if the phase of that touch equals Began
+                if (myTouch.phase == TouchPhase.Began)
+                {
+                    //If so, set touchOrigin to the position of that touch
+                    touchOrigin = myTouch.position;
+                }
+
+                //If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+                else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+                {
+                    //Set touchEnd to equal the position of this touch
+                    Vector2 touchEnd = myTouch.position;
+
+                    //Calculate the difference between the beginning and end of the touch on the x axis.
+                    float x = touchEnd.x - touchOrigin.x;
+
+                    //Calculate the difference between the beginning and end of the touch on the y axis.
+                    float y = touchEnd.y - touchOrigin.y;
+
+                    //Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
+                    touchOrigin.x = -1;
+
+                    //Check if the difference along the x axis is greater than the difference along the y axis.
+                    if (Mathf.Abs(x) > Mathf.Abs(y))
+                        //If x is greater than zero, set horizontal to 1, otherwise set it to -1
+                        horizontal = x > 0 ? 1 : -1;
+                    else
+                        //If y is greater than zero, set horizontal to 1, otherwise set it to -1
+                        vertical = y > 0 ? 1 : -1;
+                }
+            }
+
+#endif //End of mobile platform dependendent compilation section started above with #elif
+		//Check if we have a non-zero value for horizontal or vertical
+		if (horizontal != 0 || vertical != 0)
+		{
+			//Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
+			//Pass in horizontal and vertical as parameters to specify the direction to move Player in.
+			AttemptMove<Wall>(horizontal, vertical);
+		}
 	}
+
 
 	protected override void AttemptMove<T>(int xDir, int yDir)
 	{
 		food--;
+		foodText.text = "Energía: " + food;
 		base.AttemptMove<T>(xDir, yDir);
 		RaycastHit2D hit;
+		if (Move(xDir, yDir, out hit))
+		{
+			SoundManager.instance.RandomizeSfx(moveSound1, moveSound2);
+		}
 		CheckIfGameOver();
 		GameManager.instance.playersTurn = false;
 	}
@@ -55,7 +136,11 @@ public class Player : MovingObject {
 	private void CheckIfGameOver()
 	{
 		if (food <= 0)
+		{
+			SoundManager.instance.PlaySingle(gameOverSound);
+			SoundManager.instance.musicSource.Stop();
 			GameManager.instance.GameOver();
+		}
 	}
 
 	private void OnTriggerEnter2D(Collider2D other)
@@ -68,11 +153,15 @@ public class Player : MovingObject {
 		else if (other.tag == "Food")
 		{
 			food += pointsPerFood;
+			foodText.text = "+" + pointsPerFood + " Energía: " + food;
+			SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
 			other.gameObject.SetActive(false);
 		}
 		else if (other.tag == "Soda")
 		{
 			food += pointsPerSoda;
+			foodText.text = "+" + pointsPerSoda + " Energía: " + food;
+			SoundManager.instance.RandomizeSfx(drinkSound1, drinkSound2);
 			other.gameObject.SetActive(false);
 		}
 	}
@@ -92,6 +181,7 @@ public class Player : MovingObject {
 	{
 		animator.SetTrigger("playerHit");
 		food -= loss;
+		foodText.text = "-" + loss + " Energía: " + food;
 		CheckIfGameOver();
 	}
 }
